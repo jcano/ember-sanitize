47144381f80e7330c2e0e069a0ae938d2a24d036
Jay Cano <inaki.cano@gmail.com>
2016-05-21 12:10:59 +0100

    Removed user configuration in favor of the three configurations included with sanitize.js

--- app/helpers/sanitize-html.js
+++ app/helpers/sanitize-html.js
@@ -1,14 +1,12 @@
-import { sanitize } from 'ember-sanitize/utils/sanitize';
+import {
+  sanitize
+} from 'ember-sanitize/utils/sanitize';
 import getOwner from 'ember-getowner-polyfill';
 import Ember from 'ember';
 
 export default Ember.Helper.extend({
   compute(params) {
-    let config, configName = params[1];
-    if (configName) {
-      //lookup the config
-      config = getOwner(this).lookup('sanitizer:' + configName);
-    }
+    let config = params[1];
 
     let sanitized = sanitize(params[0], config);
     return new Ember.String.htmlSafe(sanitized);
